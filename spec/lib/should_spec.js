ce4e62ae05d78ae8b3183cf3fe0401e6aeb4fe14
ecavazos <ejcavazos@gmail.com>
2010-01-31 01:41:13 -0800

    adding more specs

--- spec/lib/should_spec.js
+++ spec/lib/should_spec.js
@@ -9,7 +9,7 @@ describe("Should", function () {
   });
 
   it("should throw when shouldEqual fails", function () {
-    value(function() {
+    value(function () {
       ({}).shouldEqual({ bar: "bar" });
     })
     .shouldThrow()
@@ -17,17 +17,49 @@ describe("Should", function () {
   });
 
   it("should not throw when shouldNotEqual passes", function () {
-    value(function() {
+    value(function () {
       ({}).shouldNotEqual({ bar: "bar" });
     })
     .shouldNotThrow();
   });
 
   it("should throw when shouldNotEqual fails", function () {
-    value(function() {
+    value(function () {
       ({}).shouldNotEqual({});
     })
     .shouldThrow()
-    .exception('Expected:\n{}\n\nActual:\n{}');
+    .exception("Expected:\n{}\n\nActual:\n{}");
+  });
+
+  it("should not throw when shouldBeSame passes", function () {
+    value(function () {
+      var foo = 1;
+      (foo).shouldBeSame(foo);
+    })
+    .shouldNotThrow();
+  });
+
+  it("should throw when shouldBeSame fails", function () {
+    value(function () {
+      ({}).shouldBeSame({});
+    })
+    .shouldThrow()
+    .exception("Expected {} to be the same as {}");
+  });
+
+  it("should not throw when shouldNotBeSame passes", function () {
+    value(function () {
+      ({}).shouldNotBeSame({});
+    })
+    .shouldNotThrow();
+  });
+
+  it("should throw when shouldNotBeSame fails", function () {
+    value(function () {
+      var foo = 1;
+      (foo).shouldNotBeSame(foo);
+    })
+    .shouldThrow()
+    .exception("Expected 1 to not be the same as 1");
   });
 });
