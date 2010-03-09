58382ef552cb1e17766ed2fbcb3450491e85fa90
ecavazos <ejcavazos@gmail.com>
2010-03-09 07:12:40 -0800

    cleaning the scope in which specs are evaluated

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
+* Clean scope that specs are evaluated in
+  * hide included modules from specs
+* Fix broken output
 * Show the name of the spec file before showing the errors
 * Consolidate specs
 * Update README to show new API
--- spec/lib/collector_spec.js
+++ spec/lib/collector_spec.js
@@ -6,9 +6,14 @@ describe("Collector", function () {
   });
 
   it("should invoke callback with a single element array when a filename is provided", function () {
+    var callCount = 0
+
     collector.collect("", "fake_spec.js", function (err, paths) {
+      callCount++;
       value(paths).shouldEqual(["fake_spec.js"]);
     });
+
+    value(callCount).shouldEqual(1);
 
   });
 
