387f7c57128d591f5bdccda8b6e1b76dccaf2ece
ecavazos <ejcavazos@gmail.com>
2010-03-10 20:53:00 -0800

    super awesome scope cleaning insanity

--- spec/lib/results_spec.js
+++ spec/lib/results_spec.js
@@ -6,12 +6,12 @@ describe("Results", function () {
   });
 
   it("should return only the summary when the spec run contains no failures", function () {
-    var actual = results.format(2, []);
+    var actual = results.display(2, []);
     value(actual).shouldEqual("\033[32m2 specs, 2 passed, 0 failed\033[37m");
   });
 
   it("should you spec (singular form) when only one spec is run", function () {
-    var actual = results.format(1, []);
+    var actual = results.display(1, []);
     value(actual).shouldEqual("\033[32m1 spec, 1 passed, 0 failed\033[37m");
   });
 
@@ -19,13 +19,13 @@ describe("Results", function () {
     var failures = ["fail msg 1", "fail msg 2"];
     var expected = "1) fail msg 12) fail msg 2\n\033[31m2 specs, 0 passed, 2 failed\033[37m";
 
-    var actual = results.format(2, failures);
+    var actual = results.display(2, failures);
     value(actual).shouldEqual(expected);
   });
 
   it("should output failed and passed feedback", function () {
     var expected = "1) fail\n\033[31m2 specs, 1 passed, 1 failed\033[37m";
-    var actual = results.format(2, ["fail"]);
+    var actual = results.display(2, ["fail"]);
     value(actual).shouldEqual(expected);
   });
 });
